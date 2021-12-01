a75b6723edc0c14d55fcf56e2e0bb1a473c0b11a
Belén Pinilla <m.pinilla04@ufromail.cl>
2021-12-01 10:37:43 -0300

    10:30 -Clase Ventana sin terminar. -Clase VentanaActualizar sin terminar. -Clase Iniciar, Geofisicos, Persona y Desaparecidos sin terminar. -Creación Clase ArrayList.

--- Desastres/src/main/java/GUIs/VentanaActualizar.java
+++ Desastres/src/main/java/GUIs/VentanaActualizar.java
@@ -1,4 +1,11 @@
 package GUIs;
 
+import javax.swing.*;
+
 public class VentanaActualizar {
+    private JPanel panel1;
+    private JButton actualizarButton;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
 }
